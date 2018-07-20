8cc7ff3823ddad0fa71005eddf3031439d64d991
MarxGentil <marxgentil@gmail.com>
2018-07-19 21:53:44 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,13 +7,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
+
 import javassist.tools.rmi.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+
+	//objectNotFound poderia ser qualquer nome, o nome eu escolho
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardErro> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		StandardErro err = new StandardErro(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
+	
+	//dataIntegrity poderia ser qualquer nome, o nome eu escolho
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardErro> dataIntegrity(ObjectNotFoundException e, HttpServletRequest request){
+		StandardErro err = new StandardErro(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
 }
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.nelioalves.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,6 +33,17 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId()); // chama o método find para verificar se o objeto de fato existe.
 		return repo.save(obj);
+	}
+	
+	//DataIntegrityViolationException este é o erro que dá no SpringData quando tem objetos dependentes
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 	
 }
