f446608bcf5c24dec1ae6577154a41b6c52efd26
MarxGentil <marxgentil@gmail.com>
2018-08-28 15:11:23 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,7 @@ import com.nelioalves.cursomc.security.JWTUtil;
 
 @Configuration 
 @EnableWebSecurity 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter { 
 	
 	@Autowired 
@@ -41,7 +43,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS_GET = { 
 			"/produtos/**", 
-			"/categorias/**",
+			"/categorias/**"
+	}; 
+
+	private static final String[] PUBLIC_MATCHERS_POST = { 
 			"/clientes/**"
 	}; 
 
@@ -53,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.cors().and().csrf().disable(); 
 		http.authorizeRequests() 
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll() // método post paga gravar, no nosso caso, só cliente porque clientes que querem se cadastrar pela primeira vez, estarão autorizados
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll() // método get só para consulta, quando só edita pela aplicação, pelo POSTMAN por exemplo, não será permitido
 			.antMatchers(PUBLIC_MATCHERS).permitAll() 
 			.anyRequest().authenticated(); 
--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,7 @@ public class CategoriaResource {
 	}
 	
 	//Salvar uma categoria
+	@PreAuthorize("hasAnyRole('ADMIN')") // aqui só grava se for ADMIN, cliente não pode salvar.
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO){
 		Categoria obj = service.fromDTO(objDTO);
