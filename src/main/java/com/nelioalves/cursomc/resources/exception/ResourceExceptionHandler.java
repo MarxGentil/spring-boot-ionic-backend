b9d24b05ace4c8fada2f7c0767da48ad00320e33
MarxGentil <marxgentil@gmail.com>
2018-09-19 20:12:24 -0300

    Tratando excecoes adequadamente

--- src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.nelioalves.cursomc.services.exceptions.AuthorizationException;
 import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
+import com.nelioalves.cursomc.services.exceptions.FileException;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
@@ -49,4 +53,33 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
 	}
 	
+	//file poderia ser qualquer nome, o nome eu escolho
+	@ExceptionHandler(FileException.class)
+	public ResponseEntity<StandardErro> file(FileException e, HttpServletRequest request){
+		StandardErro err = new StandardErro(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	//amazonService poderia ser qualquer nome, o nome eu escolho
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardErro> amazonService(AmazonServiceException e, HttpServletRequest request){
+		HttpStatus code = HttpStatus.valueOf(e.getErrorCode());
+		
+		StandardErro err = new StandardErro(code.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(code).body(err);
+	}
+	
+	//amazonClient poderia ser qualquer nome, o nome eu escolho
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardErro> amazonClient(AmazonClientException e, HttpServletRequest request){
+		StandardErro err = new StandardErro(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	//amazonS3 poderia ser qualquer nome, o nome eu escolho
+	@ExceptionHandler(AmazonS3Exception.class)
+	public ResponseEntity<StandardErro> amazonS3(AmazonS3Exception e, HttpServletRequest request){
+		StandardErro err = new StandardErro(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 }
