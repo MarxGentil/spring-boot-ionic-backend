7e11eac0422652d50ec3126d91271aaf9aa63b0d
MarxGentil <marxgentil@gmail.com>
2018-09-26 23:24:16 -0300

    Endpoints para buscar estados e cidades

--- src/main/java/com/nelioalves/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/EstadoRepository.java
@@ -1,12 +1,16 @@
 package com.nelioalves.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nelioalves.cursomc.domain.Estado;
 
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Integer> {
 //CAMADA DE ACESSO AOS DADOS (DAO)
-	
+	@Transactional(readOnly=true)  // faz a busca ficar mais rápida
+	public List<Estado> findAllByOrderByNome();
 }
